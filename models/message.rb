276fc1510f90c5e73e0662cb14335f3735def90a
Paul Pavlovsky <fatpaher@gmail.com>
2016-10-03 15:44:14 +0300

    Add secure link generatuion to model

--- models/message.rb
+++ models/message.rb
@@ -1,5 +1,11 @@
 class Message < Sequel::Model
   def validate
+    super
     errors.add(:name, "can't be empty") if text.nil? || text.empty?
+  end
+
+  def before_save
+    super
+    self.link = SecureRandom.hex
   end
 end
